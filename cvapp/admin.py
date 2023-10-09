0375830450a57947f33e3fba4e608356c37d7cb9
Marcus Zou <40216021+marcuszou@users.noreply.github.com>
2023-10-09 14:14:57 -0600

    Update admin.py - Customize admin.py

--- cvapp/admin.py
+++ cvapp/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from . models import Feedback
+
+# Update Admin Panel - Header
+admin.site.site_header = "Backend - CVAPP - edwardzou.com"
+admin.site.site_title = "Feedback Admin Area"
+admin.site.index_title = "Welcome to the Feedback Admin, Backend of edwardzou.com"
+
 # Register your models here.
 admin.site.register(Feedback)
 # list_display = ("name", "email")
