203339e93288316255d90d02d50ed982550f5afd
Marcus Zou <40216021+marcuszou@users.noreply.github.com>
2023-11-15 20:57:53 +0200

    Update settings.py

--- mycv/settings.py
+++ mycv/settings.py
@@ -3,7 +3,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mf@th4&vlrh77ujw1@o77mpu8na@!#-gkjbq3236xu==*+()#9'
 DEBUG = True
-X_FRAME_OPTIONS = 'ALLOW-FROM [ https://edwardzou.com/, https://www.edwardzou.com/]'
+X_FRAME_OPTIONS = ALLOW-FROM [ 'https://edwardzou.com/', 'https://www.edwardzou.com/' ]
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '10.0.0.50', '68.146.19.151', '.onrender.com', '.edwardzou.com']
 # Application definition
 INSTALLED_APPS = [
