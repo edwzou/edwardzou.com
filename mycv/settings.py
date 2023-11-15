358bef36933ff9a78cb08d33713ecce78a9e8834
Marcus Zou <40216021+marcuszou@users.noreply.github.com>
2023-11-15 21:37:19 +0200

    Update settings.py

--- mycv/settings.py
+++ mycv/settings.py
@@ -24,7 +24,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'mycv.urls'
