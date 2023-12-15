b994cb076f57dd65d8a9a2ef65620ba82c154cad
marcuszou <marcus.zou@gmail.com>
2023-12-15 16:03:45 -0700

    ver2.0.9

--- mycv/settings.py
+++ mycv/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mf@th4&vlrh77ujw1@o77mpu8na@!#-gkjbq3236xu==*+()#9'
 DEBUG = True
 # More Allowed Hosts
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '10.0.0.50', '68.146.19.151', '.onrender.com', '.edwardzou.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '68.146.19.151', '.onrender.com', '.edwardzou.com']
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -88,9 +88,10 @@ SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 
-# Redirect HTTP requests to HTTPS
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# SECURE_SSL_REDIRECT = True -- Doesn't work with Django/Docker with error: ERR_TOO_MANY_REDIRECTS
+# Redirect HTTP requests to HTTPS - Eventually issue resolved by enabling HSTS on Reverse Proxy
+
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') # Disabled for debuging ERR_CONNECTION_TIMEOUT - 2023.12.12
+# SECURE_SSL_REDIRECT = True                # Doesn't work on Django/Docker with error: ERR_TOO_MANY_REDIRECTS - 2023.12.02
 
 # FOR LOCAL DEVELOPMENT - Enable the below and disable SECURE_PROXY_SSL_HEADER please
 # CORS_REPLACE_HTTPS_REFERER      = False
